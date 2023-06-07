9dd7b9e5745d89a48594b0b517d26a0234c0c89d
Yana <yanastolberg@gmail.com>
2023-06-07 11:24:50 -0400

    Task is solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    // no roots
+  } else if (discriminant === 0) {
+    // one root
+    arr.push(-b / (2 * a));
+  } else {
+    // two roots
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+// Example usage
+console.log(solveEquation(1, 2, -3));
